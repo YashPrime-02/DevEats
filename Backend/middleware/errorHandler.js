a06f549f9befeb18890292968fef45eb1ebea7f6
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2026-02-07 20:54:27 +0530

    Update errorHandler.js

--- Backend/middleware/errorHandler.js
+++ Backend/middleware/errorHandler.js
@@ -1,9 +1,10 @@
 const errorHandler = (err, req, res, next) => {
-  console.error("❌ ERROR:", err.message);
+  console.error("❌ FULL ERROR:", err); // <-- important
 
   res.status(err.status || 500).json({
     success: false,
     message: err.message || "Internal Server Error",
+    code: err.code || null,
   });
 };
 
